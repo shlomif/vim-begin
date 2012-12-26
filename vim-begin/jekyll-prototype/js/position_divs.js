a7e0dd0ff2f9bd06de61bc3da35636ad1106bde5
Shlomi Fish <shlomif@shlomifish.org>
2012-12-26 23:44:19 +0200

    Fix the width

--- vim-begin/jekyll-prototype/js/position_divs.js
+++ vim-begin/jekyll-prototype/js/position_divs.js
@@ -2,7 +2,7 @@ function position_divs() {
     var client_w = document.body.clientWidth ;
 
     $("#content").css("width",
-        client_w - 20 - 200 - ($("#right-sidebar") ? 410 : 250) + "px"
+        client_w - 20 - ($("#right-sidebar") ? 410 : 250) + "px"
     );
 }
 
