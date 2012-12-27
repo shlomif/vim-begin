63921ef54f1e43a44ff4ef0283465736ff404d03
Shlomi Fish <shlomif@shlomifish.org>
2012-12-27 12:12:02 +0200

    Fix the sass build in the makefile.

--- vim-begin/jekyll-prototype/config.rb
+++ vim-begin/jekyll-prototype/config.rb
@@ -1,6 +1,6 @@
 # Require any additional compass plugins here.
 project_type = :stand_alone
-css_dir = "src"
+css_dir = "src/css"
 sass_dir = "sass"
 images_dir = "src/images"
 # output_style = :compressed
