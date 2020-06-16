94e6ff59ba388960eca8bbe5aedd7c35a0720bfc
Shlomi Fish <shlomif@shlomifish.org>
2020-06-16 12:25:12 +0300

    add missing file.

---
+++ vim-begin/tt2-source/lib/Shlomif/Spelling/FindFiles.pm
@@ -0,0 +1,32 @@
+package Shlomif::Spelling::FindFiles;
+
+use strict;
+use warnings;
+
+use MooX qw/late/;
+use List::MoreUtils qw/any/;
+
+use HTML::Spelling::Site::Finder;
+
+my @prunes =
+(
+    qr#tutorials/getting-started-with-vim#,
+);
+
+sub list_htmls
+{
+    my ($self) = @_;
+
+    return HTML::Spelling::Site::Finder->new(
+        {
+            root_dir => 'dest',
+            prune_cb => sub {
+                my ($path) = @_;
+                return any { $path =~ $_ } @prunes;
+            },
+        }
+    )->list_all_htmls;
+}
+
+1;
+
